570cb969be5acd51aa5883c7545f2afc6d6c43b1
Bobby White <bobby.white@salesforce.com>
2021-07-08 19:49:01 -0400

    Update dependency version

--- src/main/java/bobby/sfdc/prototype/util/SizeLimitedWriter.java
+++ src/main/java/bobby/sfdc/prototype/util/SizeLimitedWriter.java
@@ -6,7 +6,7 @@ import java.io.Writer;
 
 public class SizeLimitedWriter extends BufferedWriter {
 	public static final String SIZE_LIMIT_WOULD_BE_EXCEEDED = "Size Limit would be exceeded";
-	public static final long MEGABYTE = 1024*1024;
+	public static final long MEGABYTE = 1024L*1024L;
 	long bytesWritten=0;
 	long recordsWritten=0;
 	long lastMBAnnounced=0;
@@ -15,26 +15,28 @@ public class SizeLimitedWriter extends BufferedWriter {
 	public static final String NEWLINE_SEPARATOR = "\n"; // Hardwire Unix style for consistency across all platforms
 	public static final int NEWLINE_SEPARATOR_LEN = NEWLINE_SEPARATOR.length();
 
-	public SizeLimitedWriter(Writer out) {
+	public SizeLimitedWriter(final Writer out) {
 		super(out);
-		maxBytes=100 * MEGABYTE;
+		maxBytes = 100 * MEGABYTE;
 	}
 
 	public SizeLimitedWriter(final Writer out, final int sz) {
 		super(out, sz);
-		maxBytes=100 * MEGABYTE;
+		maxBytes = 100 * MEGABYTE;
 	}
-	
+
 	public SizeLimitedWriter(final Writer out, final long maxBytes) {
 		super(out);
-		this.maxBytes=maxBytes;
+		this.maxBytes = maxBytes;
 	}
-	public void setRecordLimit(long maxRecords) {
-		this.maxRecords=maxRecords;
+
+	public void setRecordLimit(final long maxRecords) {
+		this.maxRecords = maxRecords;
 	}
-	
+
 	/**
 	 * Convenience method that writes the line and terminates it
+	 * 
 	 * @param line
 	 * @throws IOException
 	 */
@@ -43,13 +45,13 @@ public class SizeLimitedWriter extends BufferedWriter {
 		write(line);
 		newLine();
 	}
-	
+
 	private void checkLimit(final String line) throws SizeLimitExceeded {
-		if (line==null) {
+		if (line == null) {
 			throw new IllegalArgumentException("Null line");
 		} else {
-			long targetLength = bytesWritten + line.length();
-			if ((targetLength) > maxBytes || (maxRecords > 0 && recordsWritten >= maxRecords) ) {
+			final long targetLength = bytesWritten + line.length();
+			if ((targetLength) > maxBytes || (maxRecords > 0 && recordsWritten >= maxRecords)) {
 				throw new SizeLimitExceeded(SIZE_LIMIT_WOULD_BE_EXCEEDED);
 			}
 		}
@@ -57,6 +59,7 @@ public class SizeLimitedWriter extends BufferedWriter {
 
 	/**
 	 * Keep track of bytes written
+	 * 
 	 * @param line the line of text to write
 	 **/
 	@Override
@@ -64,6 +67,7 @@ public class SizeLimitedWriter extends BufferedWriter {
 		bytesWritten += line.length();
 		super.write(line);
 	}
+
 	/**
 	 * Keep track of bytes written
 	 **/
@@ -77,11 +81,11 @@ public class SizeLimitedWriter extends BufferedWriter {
 	public double getBytesWritten() {
 		return bytesWritten;
 	}
-	
-	public class SizeLimitExceeded extends IOException {
-		private static final long serialVersionUID = -6195202197503740493L;	
 
-		public SizeLimitExceeded(String msg) {
+	public class SizeLimitExceeded extends IOException {
+		private static final long serialVersionUID = 1L;
+
+		public SizeLimitExceeded(final String msg) {
 			super(msg);
 		}
 	}
